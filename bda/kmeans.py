f42188a82bcd6271b3fcd3c3ad85d912c132b9fd
NordsKingArt <jaker.raimov@gmail.com>
2021-11-06 21:40:30 +0500

    Implemented some things

--- bda/kmeans.py
+++ bda/kmeans.py
@@ -1,26 +1,52 @@
 import matplotlib.pyplot as plt
+import os
+import random
 
 
-x = [1,5,6,8]
-y = [4,3,6,3]
+# Reading datasets
+dir_path = os.path.dirname(os.path.realpath(__file__))
+dataset = open(os.path.join(dir_path, "dataset.txt"),"r")
 
-def kmeans(n_centers):
-    # centers = {}
-    # for i in range 
-    # random.randint(0,len(x)-1)
+
+
+points = list()
+for line in dataset.readlines():
+    x_cord = line.split(" ")[0]
+    y_cord = line.split(" ")[1]
+    y_cord = y_cord.replace("\n","")
+    points.append([x_cord, y_cord])
+
+
+def kmeans(points, n_centers=0):
+    centers = []
+    centers_points = list()
+
+    for i in range(n_centers):
+        center = random.choice(points)
+        if center not in centers:
+            centers.append(center)
+
+    print(centers)
+
+
+    # for point in points: 
+        
 
 
     return [[1,2],[[3,6,2,3,5],[7,5,5,2,4]]]
 
 
-center = kmeans(1)[0]
-points = kmeans(1)[1]
-plt.scatter(center[0],center[1], c="black", s=200)
-plt.scatter(points[0],points[1], c="black", label="Group #1")
 
-plt.legend()
-plt.tight_layout()
-plt.grid()
-plt.show()
+
+
+points = kmeans(points , 2)[1]
+
+
+# plt.scatter(x,y, c="black", label="Group #1")
+
+# plt.legend()
+# plt.tight_layout()
+# plt.grid()
+# plt.show()
 
 
