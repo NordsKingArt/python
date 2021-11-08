28d6a224f2790712e954561988e80917f904ffdb
NordsKingArt <jaker.raimov@gmail.com>
2021-11-08 08:52:05 +0500

    Implementing KMeans

--- bda/kmeans.py
+++ bda/kmeans.py
@@ -17,16 +17,34 @@ for line in dataset.readlines():
     points.append([x_cord, y_cord])
 
 
+def calculate_euler_distance(point1, point2):
+    point1, point2 = [int(point1[0]),int(point1[1])],[int(point2[0]),int(point2[1])]
+    distance = ((point1[0]-point2[0])**2 + (point1[1]-point2[1])**2) ** 0.5
+    return distance
+
+
 def kmeans(points, n_centers=0):
-    centers = []
-    centers_points = list()
+    centers = list()
+    centers_points = dict()
 
     for i in range(n_centers):
         center = random.choice(points)
         if center not in centers:
             centers.append(center)
 
-    print(centers)
+
+    for point in points:
+        min_distance = calculate_euler_distance(centers[0], point)
+        min_center = 0
+        for index, center in enumerate(centers):
+            distance = calculate_euler_distance(center, point)
+            if distance < min_distance:
+                min_distance = distance
+                min_center = index
+        if min_center not in centers_points:
+            centers_points[min_center] = list()
+        centers_points[min_center].append(point)
+    print(centers_points)
 
 
     # for point in points: 
